8668e8979e10ef6a00c6860236b0c45643ea5692
saurabhbhatt <bhattsourav4@gmail.com>
2025-03-12 00:45:11 +0530

    add initial files

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import * as http from 'http'
 import express from 'express'
 
 const app = express();
+const server = http.createServer(app)
+
+
+server.listen(9000, () => console.log(`Server started at POST: 9000 `))
