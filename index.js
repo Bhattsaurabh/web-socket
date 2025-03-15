c7c2e3bd54784c28a7bdb85ea5cef8bcf724383d
saurabhbhatt <bhattsourav4@gmail.com>
2025-03-15 22:37:37 +0530

    create a server to response a client and tested

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import * as http from 'http'
 import express from 'express'
+import {Server} from 'socket.io'
+import { Socket } from 'dgram';
+import {WebSocketServer} from "ws"
 
 const app = express();
-const server = http.createServer(app)
+const port = 9000
 
+const server = app.listen(port, () => console.log(`Server is listening at: 9000 `))
+const wss = new WebSocketServer({server})
 
-server.listen(9000, () => console.log(`Server started at POST: 9000 `))
+wss.on("connection", (wss)  =>{
+        wss.on("message", (data) =>{
+            console.log("Data from client: %s", data)
+            wss.send("Thanks hero")
+    }
+    )
+})
