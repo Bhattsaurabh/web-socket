42435428bec68da9ee07dc6586c8041c5295fb63
saurabhbhatt <bhattsourav4@gmail.com>
2025-03-15 23:16:21 +0530

    create a server response and tested

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import * as http from 'http'
 import express from 'express'
 import {Server} from 'socket.io'
-import { Socket } from 'dgram';
 import {WebSocketServer} from "ws"
+import path from 'path';
 
 const app = express();
 const port = 9000
 
 const server = app.listen(port, () => console.log(`Server is listening at: 9000 `))
 const wss = new WebSocketServer({server})
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve('./public/index.html'))
+})
 
 wss.on("connection", (wss)  =>{
         wss.on("message", (data) =>{
